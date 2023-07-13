6c3e112e11e9ad3632fdd8500be3b0852848f295
yohannes <yohannesguesh01@gmail.com>
2023-07-13 04:24:09 -0700

    updated

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
-* main - A program that prints exactly Programming is like building a multilingual puzzle
+* main - A program that prints a value
 * Return 0 (Success)
 */
 int main(void)
