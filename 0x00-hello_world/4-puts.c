0e2e35abb1be4fb87205037aa48e732738257ab0
yohannes <yohannesguesh01@gmail.com>
2023-07-13 04:13:03 -0700

    updated

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,8 @@
  * main - A program that prints exactly Programming is like building a multilingual puzzle, followed by a new line.
  * return 0 
  */
-int main(void){
+int main(void)
+{
         puts("\"Programming is like building a multilingual puzzle");
         return(0);
 }
