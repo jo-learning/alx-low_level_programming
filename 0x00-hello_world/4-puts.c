d922bf3129e5ce5db839ada866b072be66ca8733
yohannes <yohannesguesh01@gmail.com>
2023-07-13 04:41:44 -0700

    updated

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,6 @@
 /**
  * main - program that prints value
  * Description: prints Programming is like building a multilingual puzzle
- * @parameter: no
  * Return: Always 0 (Success)
  */
 int main(void)
