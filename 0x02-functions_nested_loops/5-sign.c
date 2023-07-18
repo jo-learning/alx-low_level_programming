b2ef1983de47de48569454d4afc0e7af92dec3d7
yohannes <yohannesguesh01@gmail.com>
2023-07-18 12:32:03 -0700

    updated

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
- * main - program that prints value
+ * print_sign -function that prints the sign of a number
+ * @n: integer
  * Description: with proper grammar, but the outcome is a piece of art,
  * Return: Always 0 (Success)
  */
