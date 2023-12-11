bb4ba21680c6e8a6b1c5b50f97d785cefb71ddb8
Yohannes Guesh <yohannesguesh01@gmail.com>
2023-12-11 10:51:09 -0800

    updated

--- 0x18-dynamic_libraries/add.c
+++ 0x18-dynamic_libraries/add.c
@@ -12,3 +12,53 @@ int add(int a, int b)
 	return a + b;
 }
 
+/**
+ * sub - subtraction two numbers
+ * @a: number
+ * @b: number
+ *
+ * Return: sub of the numbers
+ */
+
+int sub(int a, int b)
+{
+        return a - b;
+}
+
+/**
+ * mul - multiple two numbers
+ * @a: number
+ * @b: number
+ *
+ * Return: multiple of the numbers
+ */
+
+int mul(int a, int b)
+{
+        return a * b;
+}
+
+/**
+ * div - division two numbers
+ * @a: number
+ * @b: number
+ *
+ * Return: sum of the numbers
+ */
+
+int div(int a, int b)
+{
+        return a / b;
+}
+/**
+ * mod - modulo two numbers
+ * @a: number
+ * @b: number
+ *
+ * Return: modulo of the numbers
+ */
+
+int mod(int a, int b)
+{
+	return a % b;
+}
